ee816b01afcad47d2c425d9398d661babdfcc17f
Gabriel Camacho <gabriel.hcamacho@outlook.com>
2022-11-03 15:20:49 -0300

    criacao das classes conta_poupanca, conta_especial e pessoa fisica

---
+++ src/Conta_Poupanca.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+public class Conta_Poupanca extends Conta_Comum{
+
+    private Date dt_aniversario;
+
+    public Date getDt_aniversario() {
+        return dt_aniversario;
+    }
+
+    public void setDt_aniversario(Date dt_aniversario) {
+        this.dt_aniversario = dt_aniversario;
+    }
+}
---
+++ src/Pessoa_Fisica.java
@@ -0,0 +1,5 @@
+public class Pessoa_Fisica extends Pessoa{
+
+    private long cpf_pessoa;
+    private long rg_pessoa;
+}
